d91595666947f6dda724126880074e2622519f34
Jakob Ambsdorf <jaam@di.ku.dk>
2025-08-13 16:00:22 -0400

    experiment files for initial eval

---
+++ scripts/run/initial_eval/readme.md
@@ -0,0 +1,5 @@
+These runs are to get some initial results on DINOv2 + IJEPA models on two datasets, with a longer training time (50 epochs)
+
+ViT_sizes = small, base
+datasets = dermamnist, octmnist
+epochs = 50
---
+++ scripts/run/initial_eval/vits_dinov2_diet_dermamnist.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+### General options
+### –- specify queue --
+#BSUB -q p1
+### -- set the job Name --
+#BSUB -J DIET
+### -- specify that the cores must be on the same host --
+#BSUB -R "span[hosts=1]"
+### -- ask for number of cores (default: 1) --
+#BSUB -n 12
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 6:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=20GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u jaam@di.ku.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o logs/gpu_%J.out
+# -- end of LSF options --
+
+module load python3/3.11.9
+
+# Activate conda environment
+source .venv/bin/activate
+
+export CUDA_VISIBLE_DEVICES=0
+
+# Run the training script
+python main.py \
+    --backbone dinov2 \
+    --model-size small \
+    --training-mode diet_only \
+    --label-smoothing 0.2 \
+    --dataset dermamnist \
+    --num-epochs 50 \
+    --data-root /dtu/p1/jakambs/diet/data \
+    --wandb-dir /dtu/p1/jakambs/diet/wandb \
+    --checkpoint-dir /dtu/p1/jakambs/diet/checkpoints
